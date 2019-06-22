3296d1aad08d66d1013beef18b1838956589bed3
Jonathan Spring <jon.spring@gmail.com>
2019-06-22 00:35:42 -0700

    added daily interp

--- bar_race.R
+++ bar_race.R
@@ -9,7 +9,7 @@ some_data <- tibble(
   entity = rep(LETTERS[1:10], each = mo),
   month  = rep(seq.Date(as.Date("2018-01-01"), 
                         by = "month", length.out = mo), times = 10),
-  change = rnorm(360, mean = 10, sd = 3)
+  change = rnorm(10*mo, mean = 10, sd = 3)
 ) %>%
   group_by(entity) %>%
   mutate(cuml = cumsum(change))
@@ -27,14 +27,39 @@ some_data_rank_mo <- some_data %>%
   group_by(month) %>%
   mutate(rank = min_rank(cuml)) %>%
   ungroup()
-
-some_data_rank_mo %>% View()
+# some_data_rank_mo %>% View()
 
 a <- ggplot(some_data_rank_mo, aes(x = rank)) +
-  geom_tile(aes(y = cuml/2, width = 1, height = cuml)) +
+  geom_tile(aes(y = cuml/2, width = 0.9, height = cuml)) +
   geom_text(aes(label = entity, y = 0), hjust = 1) +
   coord_flip(clip = "off") +
-  # facet_wrap(~month)
   transition_time(month)
-# a
-animate(a, nframes = 36)
+animate(a, nframes = 150)
+
+
+# data interp #####
+some_data_monthly_rates <- some_data %>%
+  group_by(entity) %>%
+  mutate(next_mo = lead(month),
+         daily_rt = change / as.numeric(next_mo - month)) %>%
+  ungroup()
+
+some_data_rank_daily <- tibble(
+  entity = rep(LETTERS[1:10], each = 744),
+  day = rep(seq.Date(as.Date("2018-01-01"), by = "day", length.out = mo*31), 
+                   times = 10)) %>%
+  left_join(some_data_monthly_rates, by = c("day" = "month", "entity")) %>%
+  fill(daily_rt) %>%
+  group_by(entity) %>%
+  mutate(cuml = cumsum(daily_rt)) %>%
+  group_by(day) %>%
+  mutate(rank = min_rank(cuml))
+  
+
+
+a <- ggplot(some_data_rank_daily, aes(x = rank)) +
+  geom_tile(aes(y = cuml/2, width = 0.9, height = cuml)) +
+  geom_text(aes(label = entity, y = 0), hjust = 1) +
+  coord_flip(clip = "off") +
+  transition_time(day)
+animate(a, nframes = 150)
